f33c80ec9783f2c2d04f01c4f79b0bfefff48d7a
Airon Kim <devaironkim@gmail.com>
2024-09-23 10:52:50 -0700

    Finish excersise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (textToReverse) {
+    let finalText = "";
 
+    while (textToReverse.length > 0) {
+        finalText += textToReverse.slice(-1);
+        textToReverse = textToReverse.slice(0, -1);
+    }
+
+    return finalText;
 };
 
 // Do not edit below this line
