569c6ad34e212abcff17bc0bfeab76ad3f5a63b3
Airon Kim <devaironkim@gmail.com>
2024-09-23 16:11:35 -0700

    Finish excersise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...itemsToRemove) {
+    // using splice
+    // for (const item of itemsToRemove) {
+    //     let index = array.length;
+    //     while (index >= 0) {
+    //         if (array[index] === item) {
+    //             array.splice(index, 1);
+    //         } else {
+    //             index--;
+    //         }
+    //     }
+    // }
+
+    // using filter
+    for (const item of itemsToRemove) {
+        array = array.filter((element) => element !== item);
+    }
+
+    return array;
+
+    // from solution
+    // return array.filter((element) => !itemsToRemove.includes(element));
 };
 
 // Do not edit below this line
