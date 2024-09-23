97ad71dc2538854945a5bea2070be4b148cfac61
Airon Kim <devaironkim@gmail.com>
2024-09-23 16:41:48 -0700

    Finish excersise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (startNumber, endNumber) {
+    if (typeof startNumber !== "number" || typeof endNumber !== "number") {
+        return "ERROR";
+    } else if (
+        startNumber % Math.floor(startNumber) !== 0 ||
+        endNumber % Math.floor(endNumber) !== 0
+    ) {
+        return "ERROR";
+    } else if (startNumber < 0 || endNumber < 0) {
+        return "ERROR";
+    }
 
+    let totalSum = 0;
+
+    if (startNumber < endNumber) {
+        for (let i = startNumber; i <= endNumber; i++) {
+            totalSum += i;
+        }
+    } else {
+        for (let i = startNumber; i >= endNumber; i--) {
+            totalSum += i;
+        }
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
