401396fffb973160c287e44a84c40aaa5f9afc5c
Airon Kim <devaironkim@gmail.com>
2024-09-23 10:41:16 -0700

    Finish excersise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (textToRepeat, numberOfTimes) {
+    if (numberOfTimes < 0) return "ERROR";
 
+    let finalText = "";
+
+    for (let i = 0; i < numberOfTimes; i++) {
+        finalText += textToRepeat;
+    }
+
+    return finalText;
 };
 
 // Do not edit below this line
