60f32bca9801aed5ccb1623187cd675daf19fa07
Airon Kim <devaironkim@gmail.com>
2024-09-30 23:21:33 -0700

    Finish excersise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        let age;
+        let oldestAge;
+
+        if (!person.yearOfDeath) {
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (!oldest.yearOfDeath) {
+            oldestAge = currentYear - oldest.yearOfBirth;
+        } else {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+
+        if (age > oldestAge) {
+            return person;
+        } else {
+            return oldest;
+        }
+    });
 };
 
 // Do not edit below this line
