b730a87e5a5c70afcabf9cd0784e14240848ed78
Airon Kim <devaironkim@gmail.com>
2024-09-30 21:57:14 -0700

    Finish excersise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const lowerCased = text.toLowerCase();
+    const unpunctuated = lowerCased.replace(/[.,/#!$%^&*;:{}=\-_`~() ]/g, "");
 
+    let lastLetterIndex = unpunctuated.length - 1;
+    for (let i = 0; i < lastLetterIndex; i++) {
+        if (unpunctuated[i] != unpunctuated[lastLetterIndex]) {
+            return false;
+        }
+        lastLetterIndex--;
+    }
+    return true;
 };
 
 // Do not edit below this line
