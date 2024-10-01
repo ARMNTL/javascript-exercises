f0b0b353fbc0bf3d94208290204ba2b779dc4891
Airon Kim <devaironkim@gmail.com>
2024-09-30 21:56:53 -0700

    Finish excersise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+    return arr.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    let f = 1;
+    for (let i = 1; i <= a; i++) {
+        f *= i;
+    }
+    return f;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
