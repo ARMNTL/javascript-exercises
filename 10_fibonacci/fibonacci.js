ad6b6d150f96f96789f758579bc8416cb0e3d655
Airon Kim <devaironkim@gmail.com>
2024-09-30 22:13:18 -0700

    Finish excersise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = Number(num);
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    if (num <= 2) return 1;
 
+    let fib = [1, 1];
+
+    for (let i = 2; i <= num; i++) {
+        const nextNum = fib[i - 1] + fib[i - 2];
+        fib.push(nextNum);
+    }
+
+    return fib[num - 1];
 };
 
 // Do not edit below this line
